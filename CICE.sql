b2fbd3f4fbcb5066d5dbc1a04d63efab90234a82
NicoCarsaniga <super_pota@hotmail.com>
2023-03-23 12:24:06 -0300

    se agrega el script de base de datos inicial de usuarios y roles

---
+++ CICE.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `role` int
+);
+
+CREATE TABLE `role` (
+  `id` int PRIMARY KEY NOT NULL,
+  `type` varchar(100)
+);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`role`) REFERENCES `role` (`id`);
